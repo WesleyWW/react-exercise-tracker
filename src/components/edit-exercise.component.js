05514ce1288211c8786bbbc513dd183eedd87a39
WesleyWW <wesleywwilson1@gmail.com>
2020-08-09 00:30:47 -0700

    backend and create-exercise component

---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class EditExercise extends Component {
+
+    render(){
+        return (
+            <div>
+                <p>Here you edit an exercise</p>
+            </div>
+        )
+    }
+
+}
